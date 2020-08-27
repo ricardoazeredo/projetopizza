5ab577c30d3f3ee682de885d7985e0d6ce3dfdbc
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-08-27 20:34:49 -0300

    add items clone, image, name, description and price

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+//encurtando comandos querySelector e querySelectorAll. 
+const query = (element)=>document.querySelector(element);
+const queryAll = (element) => document.querySelectorAll(element);
+
 pizzaJson.map((item, index)=>{
-   let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+   let pizzaItem = query('.models .pizza-item').cloneNode(true);
    //preencher as informações em pizza item.
-   document.querySelector('.pizza-area').append(pizzaItem);
+
+   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.',',')}`;
+   
+   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;   
+
+
+   query('.pizza-area').append(pizzaItem);
 });
